94388297a09f2dd4d2903bb6c684288822fdea64
Pranav Gandhi <pranavrulz@gmail.com>
2013-01-26 04:53:54 -0500

    What

--- facebook.php
+++ facebook.php
@@ -7,7 +7,27 @@ $facebook = new Facebook(array(
   'secret' => '8e5be1119b92c3b976816671974cf2be'
 ));
 
-$user = $facebook->getUser();
-print_r($user);
+$user = $facebook->getUser() or die("NOT LOGGED IN");
+$params = array('scope' => 'friends_likes');
+$loginUrl = $facebook->getLoginUrl($params);
+
+$friends_raw = $facebook->api("/me/friends");
+$friends_raw = $friends_raw['data'];
+$friends = array();
+foreach($friends_raw as $f){
+  $friends[] = $f['name'];
+}
+
+for($i = 0; $i < 10; $i++){
+  $req = "/".$friends_raw[$i]['id']."/likes";
+  $likes = $facebook->api($req);
+  for($j = 0; $j < count($likes['data']); $j++){
+    $friends[$friends_raw[$i]['name']][] = $likes['data'][$j]['name'];
+  }
+}
+
+header("Content-Type: application/json");
+echo json_encode($friends);
+
 
 ?>
--- fb.js
+++ fb.js
@@ -5,42 +5,45 @@
  * }
  */
 
-window.friends = {};
-
-/** Facebook shit */
-window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '481960308507673', // App ID
-    status     : true, // check login status
-    cookie     : true, // enable cookies to allow the server to access the session
-    xfbml      : true  // parse XFBML
-  });
-
-  /** Get access token to run queries */
-  FB.login(function(response) {
-    FB.api('/me/friends', function(response){
-      for(i in response.data){
-        var fbid = response.data[i].id;
-        name = response.data[i].name;
-        window.friends[name] = [];
-        console.log(name);
-        FB.api('/'+fbid+'/likes',function(data){
-          for(j in data.data){
-            window.friends[name].push(data.data[j].name);
-          }
-        });
-      }
-    });
-
-  }, {scope: 'user_interests user_likes friends_interests friends_likes'});
-
-};
-
 // Load the SDK Asynchronously
 (function(d){
   var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
-  js = d.createElement('script'); js.id = id; js.async = true;
+  js = d.createElement('script'); js.id = id; js.async = false;
   js.src = "//connect.facebook.net/en_US/all.js";
   d.getElementsByTagName('head')[0].appendChild(js);
 }(document));
+
+
+window.friends = {};
+
+function apicall(call){
+  var ret = "lol";
+  $.ajax({
+    url: "https://graph.facebook.com"+call,
+    data: {access_token: window.accesstoken},
+    async:false,
+    success: function(response){ret = response.data; console.log('FAIL');}
+  });
+  return ret;
+}
+
+function getLikes(fbid) {
+  console.log(FB.api("/"+fbid+"/likes"));
+}
+
+/** Facebook shit */
+FB.init({
+  appId      : '481960308507673', // App ID
+  status     : true, // check login status
+  cookie     : true, // enable cookies to allow the server to access the session
+  xfbml      : true  // parse XFBML
+});
+
+/** Get access token to run queries */
+FB.login(function(response) {
+  window.accesstoken = response.authResponse.accessToken;
+  console.log(apicall("/me"));
+}, {scope: 'user_interests user_likes friends_interests friends_likes'});
+
+
 
